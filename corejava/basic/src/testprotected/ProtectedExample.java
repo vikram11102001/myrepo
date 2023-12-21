d9c72b410c93737c51eceec323da95acc4f09b18
vikram11102001 <karthikeyavikram1101@gmail.com>
2023-12-21 14:56:00 +0530

    corejava/testprotected(21/12/2023)

---
+++ corejava/basic/src/testprotected/ProtectedExample.java
@@ -0,0 +1,13 @@
+package testprotected;
+
+//import basicprograms.Protected;
+
+public class ProtectedExample {
+
+	public static void main(String[] args) {  
+
+		//Protected obj = new Protected();  
+		//System.out.println(obj.name);  
+
+	}  
+}
