3b133b109c7260a6910b5f6a9bef1138484435e5
vikram11102001 <karthikeyavikram1101@gmail.com>
2023-12-21 14:52:09 +0530

    corejava/testdeault(21/12/2023)

---
+++ corejava/basic/src/testdefault/DefaultSample.java
@@ -0,0 +1,13 @@
+package testdefault;
+
+import basicprograms.DefaultExample;
+
+public class DefaultSample {
+
+	public static void main(String[] args) {  
+
+		DefaultExample obj = new DefaultExample();  
+		//System.out.println(obj.name);  
+
+	} 
+}
