c88b7a2afa505c13d0eafd721c4e0e50c6fe4a2d
vikram11102001 <karthikeyavikram1101@gmail.com>
2024-01-12 16:22:37 +0530

    myrepo/springbatchsample/springbatch

---
+++ springbatchsample/springbatch/SpringBatchDemo/src/main/java/com/example/demo/configurations/processor/EmployeeItemProcessor.java
@@ -0,0 +1,42 @@
+package com.example.demo.configurations.processor;
+
+import com.example.demo.dtos.EmployeeDto;
+import com.example.demo.models.Company;
+import com.example.demo.repositories.CompanyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+
+public class EmployeeItemProcessor implements ItemProcessor<EmployeeDto, EmployeeDto>
+{
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeItemProcessor.class);
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Override
+    public EmployeeDto process (EmployeeDto employeeDto) throws Exception
+    {
+        if(validateCompany(employeeDto))
+        {
+            return employeeDto;
+        }
+
+        logger.debug("Not a valid company, can't save the employee");
+        return null;
+    }
+
+    private boolean validateCompany (EmployeeDto employeeDto)
+    {
+        Optional<Company> company = companyRepository.findById(employeeDto.getCompanyId());
+        if(company.isPresent())
+        {
+            return true;
+        }
+        return false;
+    }
+}
