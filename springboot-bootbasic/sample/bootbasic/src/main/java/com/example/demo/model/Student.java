7f5494f117f0c56dd7998b2056e2540b7007fde7
vikram11102001 <karthikeyavikram1101@gmail.com>
2024-01-12 14:52:43 +0530

    myrepo/springboot-basic/sample(12-01-2024)

---
+++ springboot-bootbasic/sample/bootbasic/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+public class Student {
+
+	private int studentId;
+	private String name;
+	private String departmentName;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	
+}
---
+++ springboot-bootbasic/sample/bootbasic/src/main/java/com/google/gson/Gson.java
@@ -0,0 +1,12 @@
+package com.google.gson;
+
+import java.util.ArrayList;
+
+public class Gson {
+
+	public String toJson(ArrayList list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
